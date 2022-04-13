5440167288ffb9b3ab1ff61ecac7458b7aa0ac48
Prankush Giri <76916192+prankush-tech@users.noreply.github.com>
2022-04-13 15:05:55 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/prankushsnake/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
   },
-
-
 
 })
